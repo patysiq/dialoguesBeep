43045e75b4caa992485d16fbc70f64f3c00ccf88
Patricia <patysiq@yahoo.com.br>
2020-06-13 08:54:51 -0300

    dialogues

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,7 +2,7 @@ import Fluent
 import FluentSQLiteDriver
 import Vapor
 
-// Called before your application initializes.
+
 public func configure(_ app: Application) throws {
     // Serves files from `Public/` directory
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
